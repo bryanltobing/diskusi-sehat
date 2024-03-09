a991293765e631ad4636a7a8d64f42f63be29fbb
bryanltobing <bryantobing0@gmail.com>
2024-03-10 02:55:25 +0800

    feat: add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://baku-hantam.vercel.app',
+        permanent: false,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
